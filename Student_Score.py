66c007e68ae49c03ce3a66ab5ab5305c0e344f88
victoria940 <victoriaedu235@gmail.com>
2025-07-09 20:59:05 +0100

    Create Student_Score.py

---
+++ Student_Score.py
@@ -0,0 +1,23 @@
+'''
+COS Assignment by Edu Victoria Chinenye
+Reg no: 2024924067
+'''
+
+# Get score from user
+score = float(input("Enter student's score: "))
+
+# Grading logic
+if 70 <= score <= 100:
+    print("Grade: A")
+elif 60 <= score <= 69:
+    print("Grade: B")
+elif 50 <= score <= 59:
+    print("Grade: C")
+elif 45 <= score <= 49:
+    print("Grade: D")
+elif 40 <= score <= 44:
+    print("Grade: E")
+elif 0 <= score < 40:
+    print("Grade: F")
+else:
+    print("Invalid score")
